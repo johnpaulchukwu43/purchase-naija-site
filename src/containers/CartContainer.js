ecde608325086927948ebee16e9e08ed96768f7e
chukwujohnpaul <jchukwu@teamapt.com>
2019-06-01 19:36:47 +0100

    fixed overlaying icons on mobile view options

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -7,9 +7,12 @@ import {removeFromCart} from '../actions/cartActions'
 import {getCartTotal} from '../services'
 
 const CartContainer = ({cartList, total, symbol, removeFromCart,cartProducts}) => (
-     <li  className="onhover-div mobile-cart"><div className="cart-qty-cls">{cartProducts.length}</div>
-        <Link to={`${process.env.PUBLIC_URL}/cart`}><img src={`${process.env.PUBLIC_URL}/assets/images/icon/cart.png`} className="img-fluid" alt=""/>
-            <i className="fa fa-shopping-cart"></i></Link>
+     <li  className="onhover-div mobile-cart">
+         <div className="cart-qty-cls">{cartProducts.length}</div>
+        <Link to={`${process.env.PUBLIC_URL}/cart`}>
+            <img src={`${process.env.PUBLIC_URL}/assets/images/icon/cart.png`} className="img-fluid" alt=""/>
+            <i className="fa fa-shopping-cart"></i>
+        </Link>
         <ul className="show-div shopping-cart">
             { cartProducts.map((item,index) => (
                 <CartPage key={index} item={item} total={total} symbol={symbol} removeFromCart={() => removeFromCart(cartList,item)}  />
